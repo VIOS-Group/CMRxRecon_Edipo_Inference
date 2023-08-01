8195f633d4eb994742cd4e86287284c32425f541
YuyangXue_Ed <s2287251@ed.ac.uk>
2023-08-01 22:25:59 +0100

    First commit
    fix: Add missing newline at end of file in __init__.py and mri_data.py
    
    feat: Implement SliceDataset class with __init__, _retrieve_metadata, __len__, and __getitem__ methods
    
    feat: Implement transform_utils.py with to_tensor, normalize, normalize_instance, pad_size_tensor, and crop_to_depad functions
    
    feat: Implement transforms.py with CineSample and CineNetDataTransform classes

---
+++ pl_modules/__init__.py
@@ -0,0 +1,4 @@
+from .data_module import MriDataModule
+from .mri_module import MriModule
+from .cinenet_module import CineNetModule
+from .CRNN_cinenet_module import CRNN_CineNetModule
---
+++ utils/io.py
@@ -0,0 +1,15 @@
+import scipy.io as scio
+import os
+from pathlib import Path
+
+def save_reconstructions(recons, save_dir):
+    
+    for fname, recon in recons.items():
+        file_parts = fname.split("/")
+        out_dir = Path(os.path.join(save_dir, file_parts[-3], file_parts[-2]))
+        out_dir.mkdir(exist_ok=True, parents=True)
+        path = (out_dir / file_parts[-1]).resolve()
+        save_dict = {'img4ranking': recon.transpose(1,2,0,3)}
+        scio.savemat(file_name=str(path),
+                            mdict=save_dict,
+                            store_python_metadata=True)
